64ad5bf0ac180a956d83508d773574c1358e03b5
Fadilah Safitri <fadilahfitri41@gmail.com>
2025-12-14 11:26:39 +0700

    Update route.php

--- Admin/route.php
+++ Admin/route.php
@@ -4,26 +4,23 @@ switch($p){
     case 'dashboard':
         require_once "home.php";
         break;
-    case 'ajukan_surat':
-        require_once "pengajuan.php";
-        break;
-    case 'ajuan_saya':
-        require_once "ajuan.php";
-        break;
-    case 'form':
-        require_once "form.php";
-        break;
-    case 'hapus':
-        require_once "hapus.php";
-        break;
-    case 'edit':
-        require_once "edit.php";
-        break;
     case 'detail':
         require_once "detail.php";
         break;
     case 'logout':
         require_once "../logout.php";
+        break;
+    case 'verifikasi':
+        require_once "verifikasi.php";
+        break;
+    case 'ajuan':
+        require_once "ajuan.php";
+        break;
+    case 'surat':
+        require_once "surat.php";
+        break;
+    case 'data_warga':
+        require_once "data_warga.php";
         break;
     
 default:
